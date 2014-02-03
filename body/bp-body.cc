74bb99e5737258600f4f209e06e2a0a93a0951c5
Sam Varner <snick-a-doo@comcast.net>
2014-02-02 19:23:56 -0500

    Changed license to GPL3.

--- body/bp-body.cc
+++ body/bp-body.cc
@@ -1,3 +1,22 @@
+//  bp-body.cc - Boost.Python wrapper for libvamos-body.
+//
+//  Copyright (C) 2014 Sam Varner
+//
+//  This file is part of Vamos Automotive Simulator.
+//
+//  Vamos is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  Vamos is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with Vamos.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "Gl_Car.h"
 #include "../geometry/Three_Vector.h"
 #include "../geometry/Three_Matrix.h"
--- media/bp-media.cc
+++ media/bp-media.cc
@@ -1,3 +1,22 @@
+//  bp-media.cc - Boost.Python wrapper for libvamos-media.
+//
+//  Copyright (C) 2014 Sam Varner
+//
+//  This file is part of Vamos Automotive Simulator.
+//
+//  Vamos is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  Vamos is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with Vamos.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "../body/Gl_Car.h"
 #include "XML_Parser.h"
 
